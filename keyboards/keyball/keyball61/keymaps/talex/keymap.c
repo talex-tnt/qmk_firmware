adc5ae868390f735731a454ba09a6d4fa47786e0
Alex Tinti <alex.tinti@spindox.it>
2025-05-14 10:03:32 +0200

    chore(keymaps): update layouts

--- keyboards/keyball/keyball61/keymaps/talex/keymap.c
+++ keyboards/keyball/keyball61/keymaps/talex/keymap.c
@@ -20,6 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
+
 enum custom_keycodes {
 	GUI_TAB = SAFE_RANGE,
     CTRL_TAB,
@@ -27,35 +28,48 @@ enum custom_keycodes {
 };
 
 enum {
-    MAC_BASE_LAYER = 0,
-    MAC_MOD_LAYER = 1,
-    MAC_FN_LAYER = 2
+    // os specific layouts
+    L_MAC_BASE = 0,
+    L_MAC_MOD = 1,
+
+    // general layouts
+    L_FUNC = 2,
+    L_KEYBALL = 3
 };
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [MAC_BASE_LAYER] = LAYOUT_universal(
-    KC_ESC   , KC_1     , KC_2     , KC_3               , KC_4     , KC_5                                            , KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_BSPC ,
-    KC_TAB   , KC_Q     , KC_W     , KC_E               , KC_R     , KC_T                                            , KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_BSLS ,
-    KC_DEL   , KC_A     , KC_S     , KC_D               , KC_F     , KC_G                                            , KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_ENT  ,
-    KC_LSFT  , KC_Z     , KC_X     , KC_C               , KC_V     , KC_B              , KC_BTN1           , _______ , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT ,
-    KC_LGUI  , KC_LCTL  , KC_LALT  , MO(MAC_FN_LAYER)   , _______  , MO(MAC_MOD_LAYER) , _______           , _______ , KC_SPC   , _______  , _______  , _______  , KC_RALT  , KC_RGUI
+
+  [L_MAC_BASE] = LAYOUT_universal(
+    KC_ESC   , KC_1     , KC_2     , KC_3               , KC_4     , KC_5                                                       , KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_BSPC ,
+    KC_TAB   , KC_Q     , KC_W     , KC_E               , KC_R     , KC_T                                                       , KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_BSLS ,
+    KC_DEL   , KC_A     , KC_S     , KC_D               , KC_F     , KC_G                                                       , KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_ENT  ,
+    KC_LSFT  , KC_Z     , KC_X     , KC_C               , KC_V     , KC_B              , MO(L_FUNC)        , MO(L_KEYBALL)      , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT ,
+    KC_LGUI  , KC_LCTL  , KC_LALT  , _______            , _______  , MO(L_MAC_MOD)     , KC_BTN1           , KC_RCTL            , KC_SPC   , _______  , _______  , _______  , KC_RALT  , KC_RGUI
   ),
 
-  [MAC_MOD_LAYER] = LAYOUT_universal(
-    KC_GRAVE , KC_F1    , KC_F2     , KC_F3             , KC_F4    , KC_F5                                            , KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_DEL  ,
-    _______  , KC_F11   , KC_F12    , CTRL_TAB          , _______  , _______                                          , KC_PGUP  , KC_HOME  , KC_UP    , KC_END   , KC_LBRC  , KC_RBRC ,
-    _______  , KC_CAPS  , _______   , GUI_TAB           , _______  , _______                                          , KC_PGDN  , KC_LEFT  , KC_DOWN  , KC_RGHT  , KC_BSPC  , KC_COMM ,
-    _______  , _______  , _______   , _______           , _______  , _______           , KC_BTN2           , _______  , _______  , _______  , _______  , _______  , _______  , _______ ,
-    _______  , _______  , _______   , _______           , _______  , _______           , _______           , _______  , _______  , _______  , _______  , _______  , _______  , _______
+  [L_MAC_MOD] = LAYOUT_universal(
+    KC_GRAVE , _______  , _______   , _______           , _______  , _______                                                    , _______  , _______  , _______  , _______  , _______  , KC_DEL  ,
+    _______  , _______  , _______   , CTRL_TAB          , _______  , _______                                                    , KC_PGUP  , KC_HOME  , KC_UP    , KC_END   , KC_LBRC  , KC_RBRC ,
+    _______  , KC_CAPS  , _______   , GUI_TAB           , _______  , _______                                                    , KC_PGDN  , KC_LEFT  , KC_DOWN  , KC_RGHT  , KC_BSPC  , KC_COMM ,
+    _______  , _______  , _______   , _______           , _______  , KC_BTN3           , KC_BTN2           , _______            , _______  , _______  , _______  , _______  , _______  , _______ ,
+    _______  , _______  , _______   , _______           , _______  , _______           , _______           , _______            , _______  , _______  , _______  , _______  , _______  , _______
   ),
 
-  [MAC_FN_LAYER] = LAYOUT_universal(
-    RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  , _______  ,                                  RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
-    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , _______  ,                                  RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  , _______  ,
-    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , _______  ,                                  CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE , KBC_RST  ,
-    _______  , _______  , SCRL_DVD , SCRL_DVI , SCRL_MO  , SCRL_TO  , EE_CLR   ,            EE_CLR   , KC_HOME  , KC_PGDN  , KC_PGUP  , KC_END   , _______  , _______  ,
-    QK_BOOT  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , KC_BSPC  , _______  , _______  , _______  , _______  , QK_BOOT
+  [L_FUNC] = LAYOUT_universal(
+    _______  , KC_F1    , KC_F2     , KC_F3             , KC_F4    , KC_F5                                                      , KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , _______ ,
+    _______  , KC_F11   , KC_F12    , _______           , _______  , _______                                                    , _______  , _______  , _______  , _______  , _______  , _______ ,
+    _______  , _______  , _______   , _______           , _______  , _______                                                    , _______  , _______  , _______  , _______  , _______  , _______ ,
+    _______  , _______  , _______   , _______           , _______  , _______           , _______           , _______            , _______  , _______  , _______  , _______  , _______  , _______ ,
+    _______  , _______  , _______   , _______           , _______  , _______           , _______           , _______            , _______  , _______  , _______  , _______  , _______  , _______
+  ),
+
+  [L_KEYBALL] = LAYOUT_universal(
+    RGB_TOG  , AML_TO   , AML_I50  , AML_D50            , _______  , _______                                                    , RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K ,
+    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI            , _______  , _______                                                    , RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  , _______ ,
+    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD            , _______  , _______                                                    , CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE , KBC_RST ,
+    _______  , _______  , SCRL_DVD , SCRL_DVI           , SCRL_MO  , SCRL_TO            , EE_CLR            , _______           , KC_HOME  , KC_PGDN  , KC_PGUP  , KC_END   , _______  , _______ ,
+    QK_BOOT  , _______  , _______  , _______            , _______  , _______            , _______           , _______           , KC_BSPC  , _______  , _______  , _______  , _______  , QK_BOOT
   )
 
 };
@@ -63,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is MAC_MOD_LAYER
-    keyball_set_scroll_mode(get_highest_layer(state) == MAC_MOD_LAYER);
+    keyball_set_scroll_mode(get_highest_layer(state) == L_MAC_MOD);
     return state;
 }
 
