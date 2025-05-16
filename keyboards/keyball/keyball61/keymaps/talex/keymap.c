5205db9e84dc3bfe92c526b0d3b051a689c3b538
Alex Tinti <alex.tinti@spindox.it>
2025-05-16 10:07:40 +0200

    fix: use low tap with mod layer and button click

--- keyboards/keyball/keyball61/keymaps/talex/keymap.c
+++ keyboards/keyball/keyball61/keymaps/talex/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_Q     , KC_W              , KC_E           , KC_R        , KC_T                                                               , KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_BSLS ,
     KC_DEL   , KC_A     , KC_S              , KC_D           , KC_F        , KC_G                                                               , KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
     KC_LSFT  , KC_Z     , KC_X              , KC_C           , KC_V        , KC_B              , MO(L_FUNC)             , MO(L_KEYBALL)         , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT ,
-    KC_LGUI  , KC_LCTL  , KC_LALT           , _______        , _______     , MO(L_MAC_MOD)     , KC_BTN1                , MT(MOD_RCTL, KC_ENT)  , KC_SPC   , _______  , _______  , _______  , KC_RALT  , KC_RGUI
+    KC_LGUI  , KC_LCTL  , KC_LALT           , _______        , _______     , LT(L_MAC_MOD,KC_BTN1)     , KC_BTN1                , MT(MOD_RCTL, KC_ENT)  , KC_SPC   , _______  , _______  , _______  , KC_RALT  , KC_RGUI
   ),
 
   [L_MAC_MOD] = LAYOUT_universal(
