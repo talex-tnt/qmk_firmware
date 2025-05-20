bb1fa52ac0d59f233747b0ea3131689a3d79ad69
Alex Tinti <alex.tinti@spindox.it>
2025-05-20 10:06:11 +0200

    fix: keymap

--- keyboards/keyball/keyball61/keymaps/talex/keymap.c
+++ keyboards/keyball/keyball61/keymaps/talex/keymap.c
@@ -43,11 +43,11 @@ enum {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [L_MAC_BASE] = LAYOUT_universal(
-    KC_ESC   , KC_1     , KC_2              , KC_3           , KC_4                     , KC_5                                                               , KC_6     , KC_7           , KC_8     , KC_9           , KC_0     , KC_BSPC ,
-    KC_TAB   , KC_Q     , KC_W              , KC_E           , KC_R                     , KC_T                                                               , KC_Y     , KC_U           , KC_I     , KC_O           , KC_P     , KC_BSLS ,
-    KC_DEL   , KC_A     , KC_S              , KC_D           , KC_F                     , KC_G                                                               , KC_H     , KC_J           , KC_K     , KC_L           , KC_SCLN  , KC_ENT  ,
-    KC_LSFT  , KC_Z     , KC_X              , KC_C           , KC_V                     , KC_B           , _______                   , MO(L_KEYBALL)         , KC_N     , KC_M           , KC_COMM  , KC_DOT         , KC_SLSH  , KC_RSFT ,
-    KC_LGUI  , KC_LCTL  , KC_LALT           , _______        , LT(L_FUNC, KC_BTN1)      , MO(L_MAC_MOD)  , KC_SPC                   , MOD_RCTL              , KC_SPC   , _______        , _______  , _______        , KC_RALT  , KC_RGUI
+    KC_ESC   , KC_1     , KC_2              , KC_3           , KC_4        , KC_5                                                              , KC_6     , KC_7           , KC_8     , KC_9           , KC_0     , KC_BSPC ,
+    KC_TAB   , KC_Q     , KC_W              , KC_E           , KC_R        , KC_T                                                              , KC_Y     , KC_U           , KC_I     , KC_O           , KC_P     , KC_BSLS ,
+    KC_DEL   , KC_A     , KC_S              , KC_D           , KC_F        , KC_G                                                              , KC_H     , KC_J           , KC_K     , KC_L           , KC_SCLN  , KC_ENT  ,
+    KC_LSFT  , KC_Z     , KC_X              , KC_C           , KC_V        , KC_B           , _______                  , MO(L_KEYBALL)         , KC_N     , KC_M           , KC_COMM  , KC_DOT         , KC_SLSH  , KC_RSFT ,
+    KC_LGUI  , KC_LCTL  , KC_LALT           , KC_BTN1        , L_FUNC      , MO(L_MAC_MOD)  , KC_SPC                   , MOD_RCTL              , KC_SPC   , _______        , _______  , _______        , KC_RALT  , KC_RGUI
   ),
 
   [L_MAC_MOD] = LAYOUT_universal(
@@ -177,7 +177,7 @@ bool handle_alt_gui_tab(uint16_t keycode, keyrecord_t *record) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if(layer_state_is(L_MAC_MOD) && !swap_alt_gui(keycode, record)) {
+    if(!swap_alt_gui(keycode, record)) {
         return false;
     }
     if(layer_state_is(L_MAC_MOD) && !swap_ctrl_gui(keycode, record)) {
